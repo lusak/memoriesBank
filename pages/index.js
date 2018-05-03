ba0d4a5fb10e678f18808d478c929cc7b8fd2b97
Michal Lus <michallus88@gmail.com>
2018-05-04 00:30:42 +0200

    Added creation of MemoryBank Account

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
-import { Grid, GridColumn, Card, CardContent, CardHeader, Button, GridRow, Header, Input } from 'semantic-ui-react';
+import { Grid, GridColumn, Card, CardContent, CardHeader, Button, GridRow, Header, Input, Message } from 'semantic-ui-react';
 import Layout from '../components/Layout';
+import factory from '../ethereum/factory';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 
 class MemoriesBankIndex extends Component {
+  state = {
+    errorMessage: '',
+    errorMessageVisible: 'hidden',
+    createMessageVisible: 'hidden',
+    accountAddress: '',
+    accountAddressVisible: 'hidden',
+    visible: false,
+    loading: false
+  }
+
+
+  createAccount = async (event) => {
+    event.preventDefault();
+
+    this.setState({ errorMessage: '', errorMessageVisible: 'hidden', accountAddressVisible: 'hidden',  createMessageVisible: 'hidden' });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      let accountAddress = await factory.methods.getSenderMemoriesBank().call({
+        from: accounts[0]
+      });
+
+      if (accountAddress!=='0x0000000000000000000000000000000000000000') {
+        this.setState({errorMessage: 'You already have an account. We displayed it for you below', 
+                        accountAddress: accountAddress, accountAddressVisible: 'visible', 
+                        errorMessageVisible: 'visible'});
+        return
+      }
+
+      this.setState({loading: true});
+
+      await factory.methods.createMemoriesBank().send({
+        from: accounts[0]
+      });
+
+
+      this.setState({ accountAddress: accountAddress, accountAddressVisible: 'visible', createMessageVisible: 'visible'})
+      
+    } catch (err) {
+      this.setState({errorMessage: err.message, errorMessageVisible: 'visible'});
+    }
+    this.setState({ loading: false });
+
+  }
+
   render() {
     return (
       <Layout title="Welcome to Memories Bank">
@@ -12,12 +61,12 @@ class MemoriesBankIndex extends Component {
             <Grid.Column width={8} textAlign="center">
               <Card fluid={true}>
                 <Card.Header>
-                  <Header size="large" style={{ marginTop: '10px', marginBottom: '10px' }}>
+                  <Header size="large" style={{ marginTop: '10px', marginBottom: '10px'}}>
                     New user?
                   </Header>
                 </Card.Header>
                 <Card.Content style={{ marginTop: '10px' }}>
-                  <Button color="blue">
+                  <Button loading={this.state.loading} primary onClick={this.createAccount}>
                     Create MemoriesBank
                   </Button>
                 </Card.Content>
@@ -34,7 +83,7 @@ class MemoriesBankIndex extends Component {
                   <Input size="large" placeholder='Your Account address'/>
                 </Card.Content>
                 <Card.Content>
-                  <Button color="blue">
+                  <Button color="blue" >
                     Go
                   </Button>
                 </Card.Content>
@@ -42,6 +91,11 @@ class MemoriesBankIndex extends Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
+        <Message error header="Ooops!" content={this.state.errorMessage} style={{visibility: this.state.errorMessageVisible}} />
+        <Message header="Great! Your Memories Bank has been created. Save the address below"
+                 content="You may use this address to access your memories bank in case you lose access to your ethereum account"
+                 style={{visibility: this.state.createMessageVisible}}/>
+        <Message header={this.state.accountAddress} size='huge' style={{visibility: this.state.accountAddressVisible}}/>
       </Layout>
     )
   }
--- pages/memories/new.js
+++ pages/memories/new.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Header, Form } from 'semantic-ui-react';
+import { Card, Header, Form, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class NewMemory extends Component {
@@ -13,7 +13,7 @@ class NewMemory extends Component {
       <Layout title="Add New Memory" link="/memories/show" linkname="Back">
 
         <Form>
-          <Header>Add a description:</Header>
+          <Header>Add memory description:</Header>
           <Form.TextArea placeholder="Your memory description" />
           <Header>Type of Memory:</Header>
           <Form.Group inline>
@@ -21,8 +21,9 @@ class NewMemory extends Component {
             <Form.Radio label='Neutral' value='md' checked={value === 'md'} onChange={this.handleChange} />
             <Form.Radio label='Negative' value='lg' checked={value === 'lg'} onChange={this.handleChange} />
         </Form.Group>
-
-
+          <Header>Provide date of memory in format DD-MM-YYYY</Header>
+          <Form.Input placeholder="DD-MM-YYYY"></Form.Input>
+          <Button type='submit'>Submit</Button>
         </Form>
       </Layout>
     )
